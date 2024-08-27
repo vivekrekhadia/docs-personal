c8a6fa0bd28e905b5687c24a33c99a01cef2b8e9
vivekrekhadia <vivekrekhadia007@gmail.com>
2024-08-27 14:07:11 +0530

    intial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default Home;
